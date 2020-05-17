7b51b1ec81ab011fddcf3f16f4b2713ffcecd44d
Jarell San Jose <jarellsanjose@Jarells-MBP.home>
2020-05-17 19:12:33 -0400

    updated buttons

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,48 +1,65 @@
 import React, {Component} from 'react';
-import {StyleSheet, SafeAreaView, View} from 'react-native';
+import {
+  StyleSheet,
+  SafeAreaView,
+  View,
+  ScrollView,
+  Text,
+  TextInput,
+} from 'react-native';
 import * as colors from './colors';
-import {NumButton, OrangeButton, ControlButton} from './components/Buttons';
+import {
+  NumButton,
+  OrangeButton,
+  ControlButton,
+  LightBlueButton,
+} from './components/Buttons';
 
 class Calculator extends Component {
   render() {
     return (
-      <SafeAreaView style={styles.container}>
-        <View style={styles.buttonContainer}>
-          <View style={styles.buttonRowContainer}>
-            <ControlButton label="<" />
-            <ControlButton label="> " />
-            <ControlButton label="Del" />
-            <ControlButton label="Clear" />
-          </View>
-          <View style={styles.buttonRowContainer}>
-            <NumButton label="^" />
-            <NumButton label="√ " />
-            <NumButton label="π" />
-            <NumButton label="/" />
-          </View>
-          <View style={styles.buttonRowContainer}>
-            <NumButton label="7" />
-            <NumButton label="8" />
-            <NumButton label="9" />
-            <NumButton label="x" />
-          </View>
-          <View style={styles.buttonRowContainer}>
-            <NumButton label="4" />
-            <NumButton label="5" />
-            <NumButton label="6" />
-            <NumButton label="-" />
-          </View>
-          <View style={styles.buttonRowContainer}>
-            <NumButton label="1" />
-            <NumButton label="2" />
-            <NumButton label="3" />
-            <NumButton label="+" />
-          </View>
-          <View style={styles.buttonRowContainer}>
-            <NumButton label="0" />
-            <NumButton label="." />
-            <NumButton label="(" />
-            <OrangeButton label="=" />
+      <SafeAreaView style={styles.safeArea}>
+        <View style={styles.container}>
+          <ScrollView>
+            <Text> hi </Text>
+          </ScrollView>
+          <TextInput />
+          <View style={styles.buttonContainer}>
+            <View style={styles.buttonRowContainer}>
+              <ControlButton iconName="ios-arrow-round-back" />
+              <ControlButton iconName="ios-arrow-round-forward" />
+              <ControlButton iconName="ios-backspace" />
+            </View>
+            <View style={styles.buttonRowContainer}>
+              <LightBlueButton label="^" />
+              <LightBlueButton label="√ " />
+              <LightBlueButton label="π" />
+              <LightBlueButton label="/" />
+            </View>
+            <View style={styles.buttonRowContainer}>
+              <NumButton label="7" />
+              <NumButton label="8" />
+              <NumButton label="9" />
+              <LightBlueButton label="x" />
+            </View>
+            <View style={styles.buttonRowContainer}>
+              <NumButton label="4" />
+              <NumButton label="5" />
+              <NumButton label="6" />
+              <LightBlueButton label="-" />
+            </View>
+            <View style={styles.buttonRowContainer}>
+              <NumButton label="1" />
+              <NumButton label="2" />
+              <NumButton label="3" />
+              <LightBlueButton label="+" />
+            </View>
+            <View style={styles.buttonRowContainer}>
+              <NumButton label="0" />
+              <NumButton label="." />
+              <NumButton label="(" />
+              <OrangeButton label="=" />
+            </View>
           </View>
         </View>
       </SafeAreaView>
@@ -53,16 +70,20 @@ class Calculator extends Component {
 export default Calculator;
 
 const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+  },
   container: {
     flex: 1,
     backgroundColor: colors.WHITE,
+    marginLeft: 20,
+    marginRight: 20,
+    marginBottom: 15,
   },
   buttonsContainer: {
     flex: 1,
     flexDirection: 'column',
     justifyContent: 'flex-end',
-    marginLeft: 20,
-    marginRight: 20,
   },
   buttonRowContainer: {
     flexDirection: 'row',
--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {StyleSheet, TouchableOpacity, Text} from 'react-native';
 import * as colors from '../colors';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 export const NumButton = props => (
   <TouchableOpacity style={styles.numButton}>
@@ -14,9 +15,15 @@ export const OrangeButton = props => (
   </TouchableOpacity>
 );
 
+export const LightBlueButton = props => (
+  <TouchableOpacity style={styles.lightBlueButton}>
+    <Text style={styles.text}>{props.label}</Text>
+  </TouchableOpacity>
+);
+
 export const ControlButton = props => (
   <TouchableOpacity style={styles.controlButton}>
-    <Text style={styles.controlText}>{props.label}</Text>
+    <Ionicons name={props.iconName} color={colors.WHITE} size={36} />
   </TouchableOpacity>
 );
 
@@ -25,7 +32,7 @@ const styles = StyleSheet.create({
     backgroundColor: colors.BLUE,
     flex: 1,
     margin: 5,
-    borderRadius: 10,
+    borderRadius: 25,
     paddingTop: 12,
     paddingBottom: 12,
   },
@@ -42,12 +49,23 @@ const styles = StyleSheet.create({
     paddingTop: 12,
     paddingBottom: 12,
   },
+  lightBlueButton: {
+    backgroundColor: colors.LIGHT_BLUE,
+    flex: 1,
+    margin: 5,
+    borderRadius: 25,
+    paddingTop: 12,
+    paddingBottom: 12,
+  },
   controlButton: {
     backgroundColor: colors.GREY,
     flex: 1,
-    margin: 5,
-    borderRadius: 10,
-    paddingTop: 12,
-    paddingBottom: 12,
+    marginBottom: 10,
+    marginLeft: 5,
+    marginRight: 5,
+    borderRadius: 15,
+    paddingTop: 4,
+    paddingBottom: 4,
+    alignItems: 'center',
   },
 });
